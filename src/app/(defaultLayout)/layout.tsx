d705881431bdb73abdfbc6872a5d5b162957f2e3
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2025-02-23 23:24:29 +0600

    refactor layout component to include header and sidebar for improved navigation

--- src/app/(defaultLayout)/layout.tsx
+++ src/app/(defaultLayout)/layout.tsx
@@ -1,16 +1,41 @@
-import { Metadata } from "next";
+import Header from "@/components/shared/Header";
+import Sidebar from "@/components/shared/Sidebar";
+import { Button } from "@/components/ui/button";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+import { Menu } from "lucide-react";
 
-export const metadata: Metadata = {
-  title: "Insightify",
-  description: "Transform Voice, Images, and Videos into Text",
-};
+import type React from "react"; // Import React
 
-const CommonLayout = ({ children }: { children: React.ReactNode }) => {
+export default function DashboardLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
   return (
-    <>
-      <div>{children}</div>
-    </>
-  );
-};
+    <div className="min-h-screen bg-background">
+      {/* Mobile Nav */}
+      <Sheet>
+        <SheetTrigger asChild className="lg:hidden  fixed left-4 top-4 z-50">
+          <Button className="bg-white  border" size="icon">
+            <Menu className="h-6 w-6 text-black bg-white" />
+            {/* <span className="sr-only">Toggle navigation menu</span> */}
+          </Button>
+        </SheetTrigger>
+        <SheetContent side="left" className="w-64 p-0 bg-white">
+          <Sidebar />
+        </SheetContent>
+      </Sheet>
 
-export default CommonLayout;
+      {/* Desktop Nav */}
+      <div className="hidden lg:block fixed inset-y-0 bg-[#ffffff] text-black left-0 w-64  bg-card">
+        <Sidebar />
+      </div>
+
+      {/* Main Content */}
+      <div className="lg:pl-64 h-screen overflow-y-hidden">
+        <Header />
+        <main className="p-4 rounded-tl-lg  broder h-full  overflow-y-auto ">{children}</main>
+      </div>
+    </div>
+  );
+}
