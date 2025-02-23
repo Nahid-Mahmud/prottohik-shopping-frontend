af967ca20a3e71a6bd71359906259bd58ede2190
Md. Nahid Mahmud <nahidmahmudn@gmail.com>
2025-02-24 01:22:46 +0600

    update header title on Daily Shopping page to clarify purpose

--- src/app/(defaultLayout)/add-daily-shopping/page.tsx
+++ src/app/(defaultLayout)/add-daily-shopping/page.tsx
@@ -3,7 +3,7 @@ import { ShoppingListForm } from "@/components/addDailyShopping/ShoppingListForm
 export default async function ShoppingListPage() {
   return (
     <div className="container mx-auto p-4">
-      <h1 className="text-2xl font-bold mb-4">Daily Shopping List</h1>
+      <h1 className="text-2xl font-bold mb-4">Add Todays List</h1>
       <ShoppingListForm />
     </div>
   );
